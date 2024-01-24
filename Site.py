95953ea6cb0c4c83f41963fb8139266243aa3059
Personal website <cacher300@gmail.com>
2024-01-23 21:44:12 -0400

    BIG NEW UPGRADES

--- Site.py
+++ Site.py
@@ -1,10 +1,18 @@
-from Text import texts
-import time
-from model import main
-
 
 def timer():
-    while True:
-        main()
-        texts()
-        time.sleep(1800)
+  import requests
+  from model2 import main2
+  from Text import texts
+  import time
+  from model import main
+  while True:
+      # Get the current time
+  
+    # Check if it's 3:00am
+
+      main()
+      texts()
+      time.sleep(1800) 
+          # Add any other code you want to run at this time
+  
+      # Wait for one second before checking the time again

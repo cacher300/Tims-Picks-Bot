5f8f6cb7cedebf7788bcac6eb66303a7863ff2b9
Personal website <cacher300@gmail.com>
2024-01-23 21:51:21 -0400

    BIG NEW UPGRADES

--- Site.py
+++ Site.py
@@ -1,18 +1,11 @@
-
+import time
+from model import main
 def timer():
-  import requests
-  from model2 import main2
-  from Text import texts
-  import time
-  from model import main
-  while True:
-      # Get the current time
-  
-    # Check if it's 3:00am
+    while True:
+        main()
 
-      main()
-      texts()
-      time.sleep(1800) 
-          # Add any other code you want to run at this time
-  
-      # Wait for one second before checking the time again
+        # texts()
+        # Removing tests just for now because i dont need it right now
+
+        time.sleep(1800)
+
--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import subprocess
 
 flask_process = subprocess.Popen(['python', 'Frontend.py'])
 
-    # Run timer function as a separate process
 timer_process = subprocess.Popen(['python', '-c', 'from Site import timer; timer()'])
 
-    # Wait for both processes to complete
 flask_process.wait()
 timer_process.wait()

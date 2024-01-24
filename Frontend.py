5d160f05ce78e0a4757eef67de147abbdb44845b
Personal website <cacher300@gmail.com>
2024-01-23 21:29:25 -0400

    BIG NEW UPGRADES

--- Frontend.py
+++ Frontend.py
@@ -29,8 +29,24 @@ def model1():
 
 @app.route('/model2')
 def model2():
+  data11 = []
+  data22 = []
+  data33 = []
 
-  return render_template('model2.html')
+  with open('GPT1.csv', 'r') as f1:
+      reader1 = csv.reader(f1)
+      data11 = [row for row in reader1]
+
+  with open('GPT2.csv', 'r') as f2:
+      reader2 = csv.reader(f2)
+      data22 = [row for row in reader2]
+
+  with open('GPT3.csv', 'r') as f3:
+      reader3 = csv.reader(f3)
+      data33 = [row for row in reader3]
+
+  return render_template('model2.html', data1=data11, data2=data22, data3=data33)
+
 @app.route('/model3')
 def model3():
 
--- Site.py
+++ Site.py
@@ -1,30 +1,18 @@
 
 def timer():
-  import datetime
-  import time
   import requests
-  #from Text import texts
+  from model2 import main2
+  from Text import texts
+  import time
   from model import main
-
   while True:
       # Get the current time
-      now = datetime.datetime.now()
   
-      # Check if it's 3:00am
-      if now.hour == 4 and now.minute == 5 and now.second == 40:
-  
-          url = "http://www.hockeychallengepicks.ca/"
-          response = requests.get(url)
-          
-  
-          source_code = response.text
-          with open("data.html", "w") as f:
-              f.write(source_code)
-          print("Website source code extracted")
-          main()
-          #texts()
-          #Removing texts right now bc it costs too much lol
-  
+    # Check if it's 3:00am
+
+      main()
+      texts()
+      time.sleep(1800) 
+          # Add any other code you want to run at this time
   
       # Wait for one second before checking the time again
-      time.sleep(1)
